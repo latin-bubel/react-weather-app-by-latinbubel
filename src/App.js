46b317872c40abeffd03fe4b5ef8d67091868c31
latin bubel <esterapietkiewicz0110@gmail.com>
2022-12-06 18:30:58 +0100

    Added linkedin and Github Repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project is created by{" "}
+          <a
+            href="https://pl.linkedin.com/in/estera-pietkiewicz-6b7558178"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Estera Pietkiewicz
+          </a>{" "}
+          and it is{" "}
+          <a
+            href="https://github.com/latin-bubel/react-weather-app-by-latinbubel"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open sourced
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
