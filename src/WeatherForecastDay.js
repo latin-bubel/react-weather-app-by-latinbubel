01e8d61c7fa97aa41f5d1ea86e1ce44c62780d60
latin bubel <esterapietkiewicz0110@gmail.com>
2022-12-06 19:36:49 +0100

    Updated WeatherForecastDay

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,16 +1,33 @@
 import React from "react";
 
 export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let maxTemp = Math.round(props.data.temperature.maximum);
+    return `${maxTemp}°`;
+  }
+
+  function minTemperature() {
+    let minTemp = Math.round(props.data.temperature.minimum);
+    return `${minTemp}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
   return (
     <div>
-      <div className="WeatherForecast-day"> {props.data.time}</div>
+      <div className="WeatherForecast-day"> {day()}</div>
 
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
-          {props.data.temperature.maximum}°
+          {maxTemperature()}
         </span>
         <span className="WeatherForecast-temperature-min">
-          {props.data.temperature.minimum}°
+          {minTemperature()}
         </span>
       </div>
     </div>
