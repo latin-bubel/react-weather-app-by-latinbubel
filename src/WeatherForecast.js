23730bfd904e86850b595279b8821040eddfa155
latin bubel <esterapietkiewicz0110@gmail.com>
2022-12-06 19:38:24 +0100

    Inluded LOOP to show more days of weather forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,8 +17,15 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col"></div>
-          <WeatherForecastDay data={forecast[0]} />
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
