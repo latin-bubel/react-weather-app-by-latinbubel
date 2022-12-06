55f1e10f9913c4946f483fc2ed85c40993f380e0
latin bubel <esterapietkiewicz0110@gmail.com>
2022-12-06 18:50:33 +0100

    Imported Axios

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
-        <Weather />
+        <Weather defaultCity="New York" />
 
         <footer>
           This project is created by{" "}
